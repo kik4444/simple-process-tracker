f855ec161b40d30401a276d681807d1b5085dbb0
kik4444 <7779637+kik4444@users.noreply.github.com>
2021-10-29 15:07:53 +0300

    Updated track entry and setData method

--- ui/components/trackentry.cpp
+++ ui/components/trackentry.cpp
@@ -6,10 +6,20 @@ TrackEntry::TrackEntry(QWidget *parent) : QWidget(parent), ui(new Ui::TrackEntry
     ui->setupUi(this);
 }
 
-void TrackEntry::setText(QString buttonText, QString lineText)
+QString TrackEntry::parseProcessDuration(uint processDuration)
 {
-    ui->pushButton->setText(buttonText);
-    ui->lineEdit->setText(lineText);
+    uint minutes = processDuration / 60;
+    uint hours = minutes / 60;
+    return QStringLiteral("%1").arg(hours, 2, 10, QLatin1Char('0')) + ":"
+            + QStringLiteral("%1").arg(minutes % 60, 2, 10, QLatin1Char('0')) + ":"
+            + QStringLiteral("%1").arg(processDuration % 60, 2, 10, QLatin1Char('0'));
+}
+
+void TrackEntry::setData(QString processName, uint processDuration)
+{
+    ui->lineEdit->setText(processName);
+    ui->lineEdit->setReadOnly(true);
+    ui->durationLabel->setText(parseProcessDuration(processDuration));
 }
 
 TrackEntry::~TrackEntry()
